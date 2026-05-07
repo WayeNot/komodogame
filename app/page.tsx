5283d5c555f3b184628a77b8ae063cee5236946b
baymeric <aymericbeaune917@gmail.com>
2026-05-07 09:43:49 +0200

    [ :rocket: ] - Creation des menus, reste le personnage / ennemies à faire

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,74 @@
-import Image from "next/image";
+"use client"
+import { motion } from "motion/react"
+import { style } from "motion/react-client";
+import { useState } from "react";
+import { FaGamepad, FaHamburger } from "react-icons/fa";
+import { IoSettings } from "react-icons/io5";
+import { TbLogout2 } from "react-icons/tb";
 
 export default function Home() {
-  return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+    const [displayMenu, setDisplayMenu] = useState(true)
+    const [displayGame, setDisplayGame] = useState(false)
+    const [displayBurger, setDisplayBurger] = useState(false)
+    const [displaySettings, setDisplaySettings] = useState(false)
+    const [displayGameOver, setDisplayGameOver] = useState(false)
+
+    const [dataGame, setDataGame] = useState({ score: 0 })
+
+    const [playerPos, setPlayerPos] = useState()
+
+    document.body.onkeyup = function (e) {
+        if (e.code === "space" || e.key === " " || e.code === "onclick") {
+            let player = document.getElementById("playerPos")
+            player.style.bottom = player.getBoundingClientRect().top + 10 + "px"
+        }
+    }
+
+    const startGame = () => {
+        setDisplayMenu(false);
+        setDisplayBurger(false);
+        setDisplaySettings(false);
+        setDisplayGame(true);
+    }
+
+    return (
+        <div className="h-screen flex items-center justify-center">
+            {displayMenu && (
+                <div className="w-1/4 h-[90%] rounded-[8px] m-auto flex items-center justify-center flex-col gap-10 bg-[url(https://freedesignfile.com/upload/2018/02/Underwater-world-game-background-vector-01.jpg)] bg-center bg-cover">
+                    <motion.div animate={{ rotate: [0, -5, 5, 0] }} transition={{ duration: 3, repeat: Infinity }} className="w-fit h-fit flex items-center justify-center font-bold italic gap-2">
+                        <h2 className="text-[25px] font-bold italic text-white/70">Komodo Game</h2>
+                        <img className="w-15" src="komodo.png" alt="Image du komodo" />
+                    </motion.div>
+                    <div className="flex flex-col items-center justify-center gap-3 w-full">
+                        <button onClick={startGame} className="w-2/5 py-3 bg-white/40 rounded-[8px] hover:bg-white/20 cursor-pointer transition duration-500 flex items-center gap-2 justify-center"><FaGamepad size={25} />Jouer</button>
+                        <button onClick={() => { setDisplayBurger(false); setDisplaySettings(true); }} className="w-2/5 py-3 bg-white/40 rounded-[8px] hover:bg-white/20 cursor-pointer transition duration-500 flex items-center gap-2 justify-center"><IoSettings size={25} />Paramètres</button>
+                    </div>
+                </div>
+            )}
+            {displayGame && (
+                <div className="w-1/4 relative h-[90%] rounded-[8px] m-auto flex items-center justify-center flex-col gap-10 bg-[url(https://freedesignfile.com/upload/2018/02/Underwater-world-game-background-vector-01.jpg)] bg-center bg-cover">
+                    <div className="w-full flex flex-col absolute top-0 p-3 gap-10">
+                        <div className="flex items-center justify-between w-full">
+                            <h2 className="text-[25px] font-bold italic text-white/70">KomodoGame</h2>
+                            <FaHamburger onClick={() => { setDisplayBurger(true); setDisplayGame(false); }} size={25} className="hover:text-orange-500/60 cursor-pointer transition duration-500" />
+                        </div>
+                        <motion.h2 animate={{ rotate: [0, -3, 3, 0] }} transition={{ duration: 8, repeat: Infinity }} className="text-[25px] w-full p-2 flex items-center justify-center bg-black/40 rounded-[9px]">Appuyez sur [ SPACE ] pour avancer !</motion.h2>
+                    </div>
+                    <img id="playerPos" className="w-25 absolute" src="komodo.png" alt="Image du komodo" />
+                </div>
+            )}
+            {displayBurger && (
+                <div className="w-1/4 relative h-[90%] rounded-[8px] m-auto bg-[url(https://freedesignfile.com/upload/2018/02/Underwater-world-game-background-vector-01.jpg)] bg-center bg-cover">
+                    <div className="w-full h-full flex items-center justify-center flex-col gap-10 bg-black/50">
+                        <motion.h2 animate={{ rotate: [0, -5, 5, 0] }} transition={{ duration: 3, repeat: Infinity }} className="text-[25px] font-bold italic text-white/70">KomodoGame</motion.h2>
+                        <div className="w-full flex items-center justify-center flex-col gap-2">
+                            <button onClick={() => { setDisplayBurger(false); setDisplaySettings(true); }} className="w-2/5 py-3 bg-white/40 rounded-[8px] hover:bg-white/20 cursor-pointer transition duration-500 flex items-center gap-2 justify-center"><IoSettings size={25} />Paramètres</button>
+                            <button onClick={() => { setDisplayBurger(false); setDisplayGame(true); }} className="w-2/5 py-3 bg-white/40 rounded-[8px] hover:bg-white/20 cursor-pointer transition duration-500 flex items-center gap-2 justify-center"><FaGamepad size={25} />Revenir au jeu</button>
+                            <button onClick={() => { setDisplayBurger(false); setDisplayMenu(true); }} className="w-2/5 py-3 bg-white/40 rounded-[8px] hover:bg-white/20 cursor-pointer transition duration-500 flex items-center gap-2 justify-center"><TbLogout2 size={25} />Revenir au menu</button>
+                        </div>
+                    </div>
+                </div>
+            )}
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
-    </div>
-  );
+    );
 }
